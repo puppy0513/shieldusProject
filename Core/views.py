81bbefbb7478328b81a11e6828d8f5720077a51f
jun hyun <puppy0513@naver.com>
2022-01-25 14:10:51 +0900

    First push

---
+++ Core/views.py
@@ -0,0 +1,22 @@
+from django.shortcuts import render
+from . import models
+
+# Create your views here.
+def uploadFile(request):
+    if request.method == "POST":
+        # Fetching the form data
+        fileTitle = request.POST["fileTitle"]
+        uploadedFile = request.FILES["uploadedFile"]
+
+        # Saving the information in the database
+        document = models.Document(
+            title = fileTitle,
+            uploadedFile = uploadedFile
+        )
+        document.save()
+
+    documents = models.Document.objects.all()
+
+    return render(request, "Core/upload-file.html", context = {
+        "files": documents
+    })
